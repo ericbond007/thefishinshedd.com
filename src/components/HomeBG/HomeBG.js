da0efca04ae2d9d060e7ce60666b68d219a7adbc
Eric Bond <ericbond007@gmail.com>
2018-01-16 09:57:07 -0500

    img optimizations

--- src/components/HomeBG/HomeBG.js
+++ src/components/HomeBG/HomeBG.js
@@ -6,7 +6,26 @@ class HomeBG extends Component {
   render() {
     return (
       <div className="container-fluid homeBG animated fadeInUp">
-        <Image src="sheddFrontPano-min.png" responsive alt="The Fishin Shedd"/>
+        <picture>
+ <source
+   media="(min-width: 900px)"
+   srcset="img/sheddFrontPano-lg_1x.webp 1x, img/sheddFrontPano-lg_2x.webp 2x"
+   type="image/webp" />
+ <source
+   media="(min-width: 601px)"
+   srcset="img/sheddFrontPano-md_1x.webp 1x, img/sheddFrontPano-md_2x.webp 2x"
+   type="image/webp" />
+ <source
+   srcset="img/sheddFrontPano-sm_1x.webp 1x, img/sheddFrontPano-sm_2x.webp 2x"
+   type="image/webp" />
+ <img
+   srcset="img/sheddFrontPano-sm_1x.jpg 600w,
+   img/sheddFrontPano-md_1x.jpg 900w,
+   img/sheddFrontPano-lg_1x.jpg 1440w"
+   src="image_lg_1x.jpg"
+   type="image/jpeg"
+   alt="The Fishin Shedd" />
+</picture>
       </div>
     );
   }
