8b16158f4a9f4da378ebfcb8971e210daf25385c
Eric Bond <ericbond007@gmail.com>
2018-05-13 10:17:15 -0400

    updated spring hours

--- src/components/Home/Hours.js
+++ src/components/Home/Hours.js
@@ -30,8 +30,8 @@ const Hours = () => (
                 <div className="centered">
                   <h3 className="billHeader">The Fishin Shedd  is open year round!</h3>
                     <p className="hours">Spring Hours:</p>
-                    <p className="hours">Sunday - Thursday: 6AM - 7:30PM</p>
-                    <p className="hours">Friday & Saturday: 6AM - 8:00PM or later!!</p>
+                    <p className="hours">Sunday - Thursday: 6AM - 8:30PM</p>
+                    <p className="hours">Friday & Saturday: 6AM - 9:30PM</p>
 
                     <p className="hours">Call us for more info: <a href="tel:+1-812-837-9474">812-837-9474</a></p>
                 </div>
