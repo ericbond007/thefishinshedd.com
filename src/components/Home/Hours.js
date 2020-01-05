7ba8dc407057747639a97a6b9d2a47f770a05109
Eric Bond <ericbond007@gmail.com>
2020-01-05 16:24:52 -0500

    hours updated

--- src/components/Home/Hours.js
+++ src/components/Home/Hours.js
@@ -29,9 +29,8 @@ const Hours = () => (
 
                 <div className="centered">
                   <h3 className="billHeader">The Fishin Shedd  is open year round!</h3>
-                    <p className="hours">Fall Hours:</p>
-                    <p className="hours">Sunday - Thursday: 6AM - 6:00PM</p>
-                    <p className="hours">Friday & Saturday: 6AM - 7:30PM</p>
+                    <p className="hours">Winter Hours:</p>
+                    <p className="hours">All Week: 6AM - 6:00PM</p>
                     <p className="hours">Call us for more info: <a href="tel:+1-812-837-9474">812-837-9474</a></p>
                 </div>
               </Col>
