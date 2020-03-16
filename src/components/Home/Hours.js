0a127f97f861e6feebb6d34e94d4414620bb597e
Eric Bond <ericbond007@gmail.com>
2020-03-16 09:21:55 -0400

    updated hours

--- src/components/Home/Hours.js
+++ src/components/Home/Hours.js
@@ -28,9 +28,9 @@ const Hours = () => (
   </LazyLoad>
 
                 <div className="centered">
-                  <h3 className="billHeader">The Fishin Shedd  is open year round!</h3>
-                    <p className="hours">Winter Hours:</p>
-                    <p className="hours">All Week: 6AM - 6:00PM</p>
+                  <h3 className="billHeader">The Fishin Shedd is open year round!</h3>
+                    <p className="hours">Spring Hours:</p>
+                    <p className="hours">All Week: 6AM - 7:30PM</p>
                     <p className="hours">Call us for more info: <a href="tel:+1-812-837-9474">812-837-9474</a></p>
                 </div>
               </Col>
